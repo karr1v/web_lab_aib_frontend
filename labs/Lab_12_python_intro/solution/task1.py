7e4a1e87dac4e2a1ba3b75b09b293ecf2ce6e0bc
karr1v <144384105+karr1v@users.noreply.github.com>
2023-12-24 22:24:29 +0300

    Delete labs/Lab_12_python_intro/solution/task1.py

--- labs/Lab_12_python_intro/solution/task1.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int, input().split())
-w = []
-for i in range(n):
-    w.append([0] * m)
-w[0][0] = 1
-for i in range(1, n):
-    for j in range(1, m):
-        w[i][j] = w[i-2][j-1] + w[i-1][j-2]
-print(w[-1][-1])
